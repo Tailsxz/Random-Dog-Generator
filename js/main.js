7c766e52a49d5b68eb54b33079f4cf4840dfaecc
Tai Phan <ptai7722@gmail.com>
2023-11-17 18:23:21 -0600

    added form styling, added toLowerCase() on getInput function to handle uppercased queries

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ const errorCard = document.querySelector('.card_error');
 
 //Function to return the current value of the input
 function getInput() {
-  return breedInput.value.trim();
+  return breedInput.value.toLowerCase().trim();
 }
 
 //Fetching function
